f73920eeeaffc93e858beb9c3c2f950e25ad0ea8
matthias.druwe <matthias.druwe@odisee.be>
2024-10-01 11:40:54 +0200

    interface

---
+++ Les2/CreateInterfaces/ISelectable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateInterfaces
+{
+    // Interface die ervoor zorgt we checkboxen/radiobuttons kunnen printen op het scherm. We verwachten hier een Select en Deselect functie. We willen ook weten of een item geselecteerd is of niet. ISelectable
+    internal interface ISelectable
+    {
+        void Display();
+        void Select();
+        void Deselect();
+        bool Selected { get; }
+    }
+}
